f62d1568b2a978d9a97f69024b7d631f04ccf45b
digital-nomad-cheng <vincentcheng929@gmail.com>
2023-04-13 15:39:13 +0200

    Minor updates.

--- src/main.cpp
+++ src/main.cpp
@@ -85,14 +85,14 @@ int main(int argc, char* argv[]) {
     printDesc("thrust scan, power-of-two");
     StreamCompaction::Thrust::scan(SIZE, c, a);
     printElapsedTime(StreamCompaction::Thrust::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(SIZE, c, true);
+    printArray(SIZE, c, true);
     printCmpResult(SIZE, b, c);
 
     zeroArray(SIZE, c);
     printDesc("thrust scan, non-power-of-two");
     StreamCompaction::Thrust::scan(NPOT, c, a);
     printElapsedTime(StreamCompaction::Thrust::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(NPOT, c, true);
+    printArray(NPOT, c, true);
     printCmpResult(NPOT, b, c);
 
     printf("\n");
--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -18,17 +18,19 @@ namespace StreamCompaction {
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
+            
             // TODO use `thrust::exclusive_scan`
             // example: for device_vectors dv_in and dv_out:
             // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
             thrust::device_vector<int> device_idata(idata, idata+n);
             thrust::device_vector<int> device_odata;
             device_odata.resize(n);
+            timer().startGpuTimer();
             thrust::exclusive_scan(device_idata.begin(), device_idata.end(), device_odata.begin());
+            timer().endGpuTimer();
             for (int i = 0; i < n; i++)
                 odata[i] = device_odata[i];
-            timer().endGpuTimer();
+            
         }
     }
 }
